8c0085b6de839fad07a8fac78be6474de805a253
Chris Shabsin <cshabsin@gmail.com>
2020-12-20 15:32:49 -0500

    Read day4 input into passport objects

--- 2020/4/day4.go
+++ 2020/4/day4.go
@@ -13,10 +13,43 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	for line := range ch {
-		if line.Error != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(strings.TrimSpace(*line.Contents))
+	passportChan := readPassports(ch)
+	for passport := range passportChan {
+		fmt.Println(passport.values)
 	}
+}
+
+type passport struct {
+	values map[string]string
+}
+
+func readPassports(ch chan readinp.Line) chan passport {
+	out := make(chan passport)
+	go func() {
+		p := passport{values: map[string]string{}}
+		hasContents := false
+		for line := range ch {
+			if line.Error != nil {
+				log.Fatal(line.Error)
+			}
+			thisLine := strings.TrimSpace(*line.Contents)
+			if thisLine == "" {
+				out <- p
+				p = passport{values: map[string]string{}}
+				hasContents = false
+				continue
+			}
+			hasContents = true
+			fields := strings.Fields(thisLine)
+			for _, field := range fields {
+				splitField := strings.Split(field, ":")
+				p.values[splitField[0]] = splitField[1]
+			}
+		}
+		if hasContents {
+			out <- p
+		}
+		close(out)
+	}()
+	return out
 }
