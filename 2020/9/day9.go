f1852382a2c5dd207a37ca830f20071532598510
Chris Shabsin <cshabsin@gmail.com>
2020-12-21 23:47:23 -0500

    day 9b

--- 2020/9/day9.go
+++ 2020/9/day9.go
@@ -14,6 +14,45 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	// day9a(ch)
+	target := 29221323
+	var vals []int
+	for line := range ch {
+		if line.Error != nil {
+			log.Fatal(line.Error)
+		}
+		val, err := strconv.Atoi(strings.TrimSpace(*line.Contents))
+		if err != nil {
+			log.Fatal(err)
+		}
+		vals = append(vals, val)
+	}
+	for first := 0; first < len(vals); first++ {
+		tot := 0
+		for second := first; second < len(vals); second++ {
+			tot += vals[second]
+			if tot == target {
+				min := target
+				max := 0
+				for i := first; i <= second; i++ {
+					if vals[i] < min {
+						min = vals[i]
+					}
+					if vals[i] > max {
+						max = vals[i]
+					}
+				}
+				fmt.Println(first, second, min+max)
+				return
+			}
+			if tot > target {
+				break
+			}
+		}
+	}
+}
+
+func day9a(ch chan readinp.Line) {
 	last25 := [25]int{}
 	last25set := map[int]bool{}
 	i := 0
