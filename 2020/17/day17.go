8a8727be22c24d38e7159195ae6c8ae12a50f300
Chris Shabsin <cshabsin@gmail.com>
2020-12-26 10:01:18 -0500

    Implement day 17b (4-d board)

--- 2020/17/day17.go
+++ 2020/17/day17.go
@@ -12,7 +12,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	b := board{vals: map[int]map[int]map[int]bool{}}
+	b := board{vals: map[int]map[int]map[int]map[int]bool{}}
 	row := 0
 	for line := range ch {
 		if line.Error != nil {
@@ -21,7 +21,7 @@ func main() {
 		col := 0
 		for _, c := range line.Value() {
 			if c == '#' {
-				b.set(col, row, 0, true)
+				b.set(col, row, 0, 0, true)
 			}
 			col++
 		}
@@ -33,11 +33,13 @@ func main() {
 		fmt.Println(i, b)
 	}
 	total := 0
-	for _, col := range b.vals {
-		for _, row := range col {
-			for _, alive := range row {
-				if alive {
-					total++
+	for _, space := range b.vals {
+		for _, col := range space {
+			for _, row := range col {
+				for _, alive := range row {
+					if alive {
+						total++
+					}
 				}
 			}
 		}
@@ -46,22 +48,25 @@ func main() {
 }
 
 type board struct {
-	minX, minY, minZ int
-	maxX, maxY, maxZ int
-	vals             map[int]map[int]map[int]bool
+	minX, minY, minZ, minW int
+	maxX, maxY, maxZ, maxW int
+	vals                   map[int]map[int]map[int]map[int]bool
 }
 
-func (b board) get(x, y, z int) bool {
+func (b board) get(x, y, z, w int) bool {
 	if b.vals[x] == nil {
 		return false
 	}
 	if b.vals[x][y] == nil {
 		return false
 	}
-	return b.vals[x][y][z]
+	if b.vals[x][y][z] == nil {
+		return false
+	}
+	return b.vals[x][y][z][w]
 }
 
-func (b *board) set(x, y, z int, alive bool) {
+func (b *board) set(x, y, z, w int, alive bool) {
 	if x < b.minX {
 		b.minX = x
 	}
@@ -70,6 +75,9 @@ func (b *board) set(x, y, z int, alive bool) {
 	}
 	if z < b.minZ {
 		b.minZ = z
+	}
+	if z < b.minW {
+		b.minW = w
 	}
 	if x > b.maxX {
 		b.maxX = x
@@ -80,31 +88,42 @@ func (b *board) set(x, y, z int, alive bool) {
 	if z > b.maxZ {
 		b.maxZ = z
 	}
+	if z > b.maxW {
+		b.maxW = w
+	}
 	if b.vals[x] == nil {
 		if !alive {
 			return
 		}
-		b.vals[x] = map[int]map[int]bool{}
+		b.vals[x] = map[int]map[int]map[int]bool{}
 	}
 	if b.vals[x][y] == nil {
 		if !alive {
 			return
 		}
-		b.vals[x][y] = map[int]bool{}
+		b.vals[x][y] = map[int]map[int]bool{}
 	}
-	b.vals[x][y][z] = alive
+	if b.vals[x][y][z] == nil {
+		if !alive {
+			return
+		}
+		b.vals[x][y][z] = map[int]bool{}
+	}
+	b.vals[x][y][z][w] = alive
 }
 
 func (b board) advance() board {
-	newBoard := board{vals: map[int]map[int]map[int]bool{}}
+	newBoard := board{vals: map[int]map[int]map[int]map[int]bool{}}
 	for x := b.minX - 1; x <= b.maxX+1; x++ {
 		for y := b.minY - 1; y <= b.maxY+1; y++ {
 			for z := b.minZ - 1; z <= b.maxZ+1; z++ {
-				neighbors := b.countNeighbors(x, y, z)
-				if neighbors == 3 {
-					newBoard.set(x, y, z, true)
-				} else if neighbors == 2 {
-					newBoard.set(x, y, z, b.get(x, y, z))
+				for w := b.minW - 1; w <= b.maxW+1; w++ {
+					neighbors := b.countNeighbors(x, y, z, w)
+					if neighbors == 3 {
+						newBoard.set(x, y, z, w, true)
+					} else if neighbors == 2 {
+						newBoard.set(x, y, z, w, b.get(x, y, z, w))
+					}
 				}
 			}
 		}
@@ -112,16 +131,18 @@ func (b board) advance() board {
 	return newBoard
 }
 
-func (b board) countNeighbors(x, y, z int) int {
+func (b board) countNeighbors(x, y, z, w int) int {
 	neighbors := 0
 	for dx := -1; dx <= 1; dx++ {
 		for dy := -1; dy <= 1; dy++ {
 			for dz := -1; dz <= 1; dz++ {
-				if dx == 0 && dy == 0 && dz == 0 {
-					continue
-				}
-				if b.get(x+dx, y+dy, z+dz) {
-					neighbors++
+				for dw := -1; dw <= 1; dw++ {
+					if dx == 0 && dy == 0 && dz == 0 && dw == 0 {
+						continue
+					}
+					if b.get(x+dx, y+dy, z+dz, w+dw) {
+						neighbors++
+					}
 				}
 			}
 		}
