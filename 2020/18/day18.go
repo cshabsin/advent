f3352c63b6b13719565c523521dab2f1c0b1ef8a
Chris Shabsin <cshabsin@gmail.com>
2020-12-27 17:07:11 -0500

    Implemend day18b

--- 2020/18/day18.go
+++ 2020/18/day18.go
@@ -18,7 +18,7 @@ func main() {
 		if line.Error != nil {
 			log.Fatal(line.Error)
 		}
-		val, _, err := calc(line.Value())
+		val := calc2(line.Value())
 		if err != nil {
 			fmt.Printf("error parsing line %q: %v\n", line.Value(), err)
 		}
@@ -71,4 +71,133 @@ func calc(line string) (int, int, error) {
 		operator = rune(line[off])
 		off += 2
 	}
+}
+
+type term struct {
+	parenthetical expr
+
+	// used if parenthetical is nil
+	literal int
+}
+type operator rune
+type entry struct {
+	operator
+	term
+}
+
+// expression with an implicit "0" as starting value (first term should have operator '+')
+type expr []entry
+
+// parse returns an expression and a length (to either the end of the string or the close paren)
+func parse(line string) (expr, int) {
+	fmt.Printf("parse %q\n", line)
+	off := 0
+	var rc expr
+	op := operator('+')
+	for {
+		if line[off] == '(' {
+			subexp, l := parse(line[off+1 : len(line)])
+			rc = append(rc, entry{op, term{parenthetical: subexp}})
+			off += l + 2
+		} else {
+			subval, err := strconv.Atoi(line[off : off+1])
+			if err != nil {
+				log.Fatalf("parsing digit from %q: %v", line, err)
+			}
+			rc = append(rc, entry{op, term{literal: subval}})
+			off++
+		}
+		if off == len(line) || line[off] == ')' {
+			return rc, off
+		}
+		off++
+		op = operator(line[off])
+		off += 2
+	}
+}
+
+func calc2(line string) int {
+	expr, _ := parse(line)
+	fmt.Printf("calc2(%q): %v\n", line, expr)
+	return calcExpr(expr)
+}
+
+func calcExpr(e expr) int {
+	// collapse subexpressions
+	newExpr := collapseSubExp(e)
+	fmt.Println("collapseSubexp", newExpr)
+	newExpr = collapseAdds(newExpr)
+	fmt.Println("collapseAdds", newExpr)
+	newExpr = collapseMult(newExpr)
+	fmt.Println("collapseMult", newExpr)
+	return newExpr[0].term.literal
+}
+
+func collapseSubExp(e expr) expr {
+	var newExpr expr
+	for _, ent := range e {
+		if ent.term.parenthetical != nil {
+			newExpr = append(newExpr, entry{
+				operator: ent.operator,
+				term:     term{literal: calcExpr(ent.term.parenthetical)},
+			})
+		} else {
+			newExpr = append(newExpr, ent)
+		}
+	}
+	return newExpr
+}
+
+func collapseAdds(e expr) expr {
+	var newExpr expr
+	i := 0
+	for i < len(e) {
+		val := e[i].term.literal
+		j := i + 1
+		for j < len(e) {
+			if e[j].operator == '*' {
+				break
+			}
+			if e[j].operator == '+' {
+				val += e[j].term.literal
+			}
+			if e[j].operator == '-' {
+				val -= e[j].term.literal
+			}
+			j++
+		}
+		newExpr = append(newExpr, entry{
+			operator: e[i].operator,
+			term:     term{literal: val},
+		})
+		i = j
+	}
+	return newExpr
+}
+
+func collapseMult(e expr) expr {
+	var newExpr expr
+	i := 0
+	for i < len(e) {
+		val := e[i].term.literal
+		j := i + 1
+		for j < len(e) {
+			if e[j].operator == '*' {
+				val *= e[j].term.literal
+			}
+			if e[j].operator == '+' {
+				log.Fatalf("unexpected operator + in %v", e)
+			}
+			if e[j].operator == '-' {
+				log.Fatalf("unexpected operator - in %v", e)
+			}
+			j++
+		}
+		newExpr = append(newExpr, entry{
+			operator: e[i].operator,
+			term:     term{literal: val},
+		})
+		i = j
+	}
+	return newExpr
 }
--- 2020/18/day18_test.go
+++ 2020/18/day18_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 func TestCalc(t *testing.T) {
 	testcases := []struct {
@@ -59,6 +64,116 @@ func TestCalc(t *testing.T) {
 				t.Errorf("calc len: want %d got %d", wantLen, off)
 			}
 
+		})
+	}
+}
+
+func TestParse(t *testing.T) {
+	testcases := []struct {
+		eq      string
+		want    expr
+		wantLen int
+	}{
+		{
+			eq: "1",
+			want: expr{
+				entry{
+					operator: '+',
+					term:     term{literal: 1},
+				},
+			},
+		},
+		{
+			eq: "1 + 1",
+			want: expr{
+				entry{
+					operator: '+',
+					term:     term{literal: 1},
+				},
+				entry{
+					operator: '+',
+					term:     term{literal: 1},
+				},
+			},
+		},
+		{
+			eq: "(1 + 1)",
+			want: expr{
+				entry{
+					operator: '+',
+					term: term{parenthetical: expr{
+						entry{
+							operator: '+',
+							term:     term{literal: 1},
+						},
+						entry{
+							operator: '+',
+							term:     term{literal: 1},
+						},
+					},
+					},
+				},
+			},
+		},
+		// {
+		// 	eq:      "1 + 1)",
+		// 	want:    2,
+		// 	wantLen: 5,
+		// },
+		// {
+		// 	eq:   "1 + 2 * 3",
+		// 	want: 7,
+		// },
+		// {
+		// 	eq:   "2 * 3 + 1",
+		// 	want: 7,
+		// },
+		// {
+		// 	eq:   "1 + (2 + 3)",
+		// 	want: 6,
+		// },
+		// {
+		// 	eq:   "5 + (6 + 7) + 8",
+		// 	want: 26,
+		// },
+		// {
+		// 	eq:   "((9 + 1))",
+		// 	want: 10,
+		// },
+	}
+	for _, tc := range testcases {
+		t.Run(tc.eq, func(t *testing.T) {
+			got, off := parse(tc.eq)
+			if diff := cmp.Diff(got, tc.want, cmp.AllowUnexported(entry{}, term{})); diff != "" {
+				t.Errorf("calc unexpected result, -want +got:\n%s", diff)
+			}
+			wantLen := tc.wantLen
+			if wantLen == 0 {
+				wantLen = len(tc.eq)
+			}
+			if off != wantLen {
+				t.Errorf("calc len: want %d got %d", wantLen, off)
+			}
+
+		})
+	}
+}
+
+func TestCalc2(t *testing.T) {
+	testcases := []struct {
+		eq   string
+		want int
+	}{
+		{"4 * 3 + 9", 48},
+		{"1 + 1", 2},
+		{"5 + (8 * 3 + 9 + 3 * 4 * 3)", 1445},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.eq, func(t *testing.T) {
+			fmt.Println("---", tc.eq, "---")
+			if val := calc2(tc.eq); val != tc.want {
+				t.Errorf("calc got %d want %d", val, tc.want)
+			}
 		})
 	}
 }
