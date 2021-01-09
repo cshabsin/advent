1e5ceffdc5c15fdfa0bab40e87654004e416cb61
Chris Shabsin <cshabsin@gmail.com>
2021-01-09 14:58:52 -0500

    taking a break from day20

--- 2020/20/day20.go
+++ 2020/20/day20.go
@@ -15,7 +15,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	tiles := map[int]*tile{}
+	tiles := tileMap{}
 	edgeMap := map[int][]int{}
 	for {
 		decl, err := read(ch)
@@ -29,6 +29,7 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+		tile.id = tile
 		tiles[tile] = readTile(ch)
 		for i := 0; i < 4; i++ {
 			edge := tiles[tile].readEdge(i)
@@ -40,6 +41,48 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+	// cornerTiles := day20a(tiles, edgeMap)
+
+	numMatches := map[int][]int{} // number of matches for each tile's four edges
+	for tileNum, tile := range tiles {
+		for i := 0; i < 4; i++ {
+			numMatches[tileNum] = append(numMatches[tileNum], len(edgeMap[tile.readEdge(i)])-1)
+		}
+	}
+	tileGrid := [12][12]*tile{}
+	for tile, matchList := range numMatches {
+		matchCount := 0
+		for _, matches := range matchList {
+			matchCount += matches
+		}
+		if matchCount == 2 {
+			tileGrid[0][0] = tiles[tile]
+			if matchList[0] != 0 && matchList[3] != 0 {
+				tiles[tile].rotation = 1
+			} else if matchList[3] != 0 && matchList[2] != 0 {
+				tiles[tile].rotation = 2
+			} else if matchList[2] != 0 && matchList[1] != 0 {
+				tiles[tile].rotation = 3
+			}
+			break
+		}
+	}
+	col := 1
+	row := 0
+	for {
+		if col == 0 {
+			// match up
+			nmBottom := numMatches[]
+		} else {
+
+		}
+	}
+}
+
+type tileMap map[int]*tile
+
+func day20a(tiles tileMap, edgeMap map[int][]int) []int {
+	// day 20a
 	numMatches := map[int][]int{} // number of matches for each tile's four edges
 	for tileNum, tile := range tiles {
 		for i := 0; i < 4; i++ {
@@ -47,6 +90,7 @@ func main() {
 		}
 	}
 	total := 1
+	var corners []int
 	for tile, matchList := range numMatches {
 		matches := 0
 		for _, num := range matchList {
@@ -54,116 +98,9 @@ func main() {
 		}
 		if matches == 2 {
 			total *= tile
+			corners = append(corners, tile)
 		}
 	}
 	fmt.Println(total)
-}
-
-type tile struct {
-	pixels [][]bool
-	edges  []int
-}
-
-func readTile(ch chan readinp.Line) *tile {
-	allVals := make([][]bool, 10)
-	for i := 0; i < 10; i++ {
-		line, err := read(ch)
-		if err != nil {
-			log.Fatal(err)
-		}
-		allValLine := make([]bool, 10)
-		for j, c := range line {
-			allValLine[j] = c == '#'
-		}
-		allVals[i] = allValLine
-	}
-	var edges []int
-	for i := 0; i < 4; i++ {
-		edges = append(edges, readEdge(allVals, i))
-	}
-
-	var pixels [][]bool
-	for i := 1; i < 9; i++ {
-		pixels = append(pixels, allVals[i][1:8])
-	}
-
-	return &tile{
-		pixels: pixels,
-		edges:  edges,
-	}
-}
-
-func (t tile) readEdge(e int) int {
-	return t.edges[e%4]
-}
-
-func readEdge(allVals [][]bool, e int) int {
-	switch e {
-	case 0: // top
-		total := 0
-		for j := 0; j < 10; j++ {
-			total *= 2
-			if allVals[0][j] {
-				total++
-			}
-		}
-		return total
-	case 1: // left
-		total := 0
-		for j := 9; j >= 0; j-- {
-			total *= 2
-			if allVals[j][0] {
-				total++
-			}
-		}
-		return total
-	case 2: // bottom
-		total := 0
-		for j := 9; j >= 0; j-- {
-			total *= 2
-			if allVals[9][j] {
-				total++
-			}
-		}
-		return total
-	case 3: // right
-		total := 0
-		for j := 0; j < 10; j++ {
-			total *= 2
-			if allVals[j][9] {
-				total++
-			}
-		}
-		return total
-	}
-	log.Fatal("bad e", e)
-	return -1
-}
-
-func edgeDual(a int) int {
-	var b int
-	for i := 0; i < 10; i++ {
-		b = b*2 + (a >> i & 1)
-	}
-	return b
-}
-
-func edgesMatch(a, b int) bool {
-	for i := 0; i < 10; i++ {
-		if a>>i&1 != b>>(9-i)&1 {
-			return false
-		}
-	}
-	return true
-}
-
-func read(ch chan readinp.Line) (string, error) {
-	line, ok := <-ch
-	if !ok {
-		return "", io.EOF
-	}
-	if line.Error != nil {
-		return "", line.Error
-	}
-	return strings.TrimSpace(*line.Contents), nil
+	return corners
 }
