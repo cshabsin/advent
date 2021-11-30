022088148cc7935ad45c8625ce787f1866a471d3
Chris Shabsin <cshabsin@gmail.com>
2021-11-29 22:43:49 -0500

    day 1

--- 2021/1/day1.go
+++ 2021/1/day1.go
@@ -3,5 +3,5 @@ package main
 import "github.com/cshabsin/advent/2021/1/day1"
 
 func main() {
-	day1.Day1a()
+	day1.Day1b()
 }
--- 2021/1/day1/day1.go
+++ 2021/1/day1/day1.go
@@ -1,6 +1,7 @@
 package day1
 
 import (
+	"fmt"
 	"log"
 	"strconv"
 
@@ -12,10 +13,55 @@ func Day1a() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	var increases int
+	prev := 193 // first one from input.txt
 	for line := range ch {
-		_, err := line.Get()
+		val, err := line.Get()
 		if err != nil {
 			log.Fatal(err)
 		}
+		if val > prev {
+			increases++
+		}
+		prev = val
 	}
+	fmt.Println(increases)
+}
+
+func Day1b() {
+	ch, err := readinp.Read("input.txt", strconv.Atoi)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var increases int
+	var a []int
+	for i := 0; i < 3; i++ {
+		line := <-ch
+		val, err := line.Get()
+		if err != nil {
+			log.Fatal(err)
+		}
+		a = append(a, val)
+	}
+	prev := add(a)
+	var index int
+	for line := range ch {
+		val, err := line.Get()
+		if err != nil {
+			log.Fatal(err)
+		}
+		a[index] = val
+		index = (index + 1) % 3
+		cur := add(a)
+		if cur > prev {
+			increases++
+		}
+		prev = cur
+	}
+	fmt.Println(increases)
+}
+
+func add(a []int) int {
+	fmt.Println(a)
+	return a[0] + a[1] + a[2]
 }
