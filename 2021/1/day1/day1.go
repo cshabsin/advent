a18f8ba18a9dd00830e291a8551b58d182b3ead8
Chris Shabsin <cshabsin@gmail.com>
2021-11-29 22:07:39 -0500

    prep to read input

--- 2021/1/day1/day1.go
+++ 2021/1/day1/day1.go
@@ -1,5 +1,21 @@
 package day1
 
-func Day1a() {
+import (
+	"log"
+	"strconv"
 
+	"github.com/cshabsin/advent/commongen/readinp"
+)
+
+func Day1a() {
+	ch, err := readinp.Read("input.txt", strconv.Atoi)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for line := range ch {
+		_, err := line.Get()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
