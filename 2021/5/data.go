6ed9e5327ef8ea46000b9850adcb977d21c9d3fc
Chris Shabsin <cshabsin@gmail.com>
2021-12-05 00:03:21 -0500

    data from day5

--- 2021/5/data.go
+++ 2021/5/data.go
@@ -1,7 +1,32 @@
 package main
 
-type data string
+import (
+	"log"
+	"strconv"
+	"strings"
+)
 
-func parse(line string) (string, error) {
-	return line, nil
+type data struct {
+	x1, y1 int
+	x2, y2 int
+}
+
+func atoi(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return i
+}
+
+func parse(line string) (*data, error) {
+	pairs := strings.Split(line, " -> ")
+	first := strings.Split(pairs[0], ",")
+	second := strings.Split(pairs[1], ",")
+	return &data{
+		x1: atoi(first[0]),
+		y1: atoi(first[1]),
+		x2: atoi(second[0]),
+		y2: atoi(second[1]),
+	}, nil
 }
