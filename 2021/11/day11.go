150db924c7d36aa9a539a8d3c5c357d3449d37e1
Chris Shabsin <cshabsin@gmail.com>
2021-12-11 20:55:17 -0500

    port day11 to common board

--- 2021/11/day11.go
+++ 2021/11/day11.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"strconv"
 
+	"github.com/cshabsin/advent/commongen/board"
 	"github.com/cshabsin/advent/commongen/readinp"
 )
 
@@ -16,27 +17,38 @@ func main() {
 	// day11b("input.txt")
 }
 
+type intS int
+
+func (i intS) String() string {
+	return strconv.Itoa(int(i))
+}
+
+func (intS) Delimiter() string {
+	return "" // no space between entries
+}
+
 func day11a(fn string) {
 	ch, err := readinp.Read(fn, parse)
 	if err != nil {
 		log.Fatal(err)
 	}
-	var board board
+	var brd board.Board[intS]
 	for line := range ch {
 		row, err := line.Get()
 		if err != nil {
 			log.Fatal(err)
 		}
-		board = append(board, row)
+		brd = append(brd, row)
 	}
 	var flashes100 int
 	var i int
 	for {
 		i++
 		var f int
-		f, board = board.next()
+		f, brd = nextBoard(brd)
 		if i < 100 {
 			flashes100 += f
+			fmt.Println(brd)
 		}
 		if f == 100 {
 			break
@@ -44,90 +56,29 @@ func day11a(fn string) {
 	}
 	fmt.Println(flashes100, i)
 }
-
-type board [][]int
-
-func (b board) String() string {
-	var s string
-	for r := range b {
-		for c := range b[r] {
-			s += strconv.Itoa(b[r][c])
-		}
-		s += "\n"
-	}
-	return s
-}
-
-func (b board) get(r, c int) int {
-	if r < 0 || c < 0 {
-		return 0 // the outer edge has no energy
-	}
-	if r >= len(b) {
-		return 0
-	}
-	if c >= len(b[r]) {
-		return 0
-	}
-	return b[r][c]
-}
-
-func (b *board) set(r, c, val int) {
-	if r < 0 || c < 0 {
-		return
-	}
-	if r >= len(*b) {
-		return
-	}
-	if c >= len((*b)[r]) {
-		return
-	}
-	(*b)[r][c] = val
-}
-
-func (b board) getCoord(co coord) int {
-	return b.get(co.r(), co.c())
-}
-
-type coord int
-
-func makeCoord(r, c int) coord {
-	return coord(r*1000 + c)
-}
-
-func (c coord) r() int {
-	return int(c / 1000)
-}
-
-func (c coord) c() int {
-	return int(c % 1000)
-}
-
-func (b board) next() (int, board) {
-	var next board
+func nextBoard(b board.Board[intS]) (int, board.Board[intS]) {
+	var next board.Board[intS]
 	var flashes int
 	for r := range b {
-		next = append(next, make([]int, len(b[r]), len(b[r])))
+		next = append(next, make([]intS, len(b[r]), len(b[r])))
 		for c := range b[r] {
 			next[r][c] = b[r][c] + 1
 		}
 	}
 	for {
 		var changed bool
-		for r := range b {
-			for c := range b[r] {
-				if next.get(r, c) < 10 {
-					continue
+		for _, co := range b.AllCoords() {
+			if next.GetCoord(co) < 10 {
+				continue
+			}
+			next.SetCoord(co, 0)
+			flashes++
+			changed = true
+			for _, dco := range b.Neighbors8(co) {
+				if next.GetCoord(dco) == 0 {
+					continue // already exploded
 				}
-				next.set(r, c, 0)
-				flashes++
-				changed = true
-				for _, diff := range [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}} {
-					dr, dc := diff[0], diff[1]
-					if next.get(r+dr, c+dc) == 0 {
-						continue // already exploded
-					}
-					next.set(r+dr, c+dc, next.get(r+dr, c+dc)+1)
-				}
+				next.SetCoord(dco, next.GetCoord(dco)+1)
 			}
 		}
 		if !changed {
@@ -137,13 +88,13 @@ func (b board) next() (int, board) {
 	return flashes, next
 }
 
-func parse(line string) ([]int, error) {
-	var d []int
+func parse(line string) ([]intS, error) {
+	var d []intS
 	for _, c := range line {
 		if c < '0' || c > '9' {
 			return nil, fmt.Errorf("in line %q, illegal character %c", line, c)
 		}
-		d = append(d, int(c-'0'))
+		d = append(d, intS(c-'0'))
 	}
 	return d, nil
 }
---
+++ commongen/board/board.go
@@ -0,0 +1,138 @@
+package board
+
+import (
+	"fmt"
+)
+
+type Coord int
+
+func MakeCoord(r, c int) Coord {
+	return Coord(r*1000 + c)
+}
+
+func (c Coord) R() int {
+	return int(c / 1000)
+}
+
+func (c Coord) C() int {
+	return int(c % 1000)
+}
+
+func (c Coord) Apply(diff [2]int) Coord {
+	return MakeCoord(c.R()+diff[0], c.C()+diff[1])
+}
+
+type delimiterHaver interface {
+	Delimiter() string
+}
+
+func delim(str fmt.Stringer) string {
+	d, ok := str.(delimiterHaver)
+	if !ok {
+		return " " // default delimiter
+	}
+	return d.Delimiter()
+}
+
+type Board[T fmt.Stringer] [][]T
+
+// String renders the board in string.
+//
+// This implementation assumes that the String() function
+// for the type returns values of the same width, and there
+// is no need for any delimiter
+// (designed for the one-digit boards from 2021 day 9 and 11).
+func (b Board[T]) String() string {
+	var s string
+	for r := range b {
+		for c := range b[r] {
+			var v fmt.Stringer
+			v = b[r][c]
+			s += v.String() + delim(b[r][c])
+		}
+		s += "\n"
+	}
+	return s
+}
+
+func (b Board[T]) Get(r, c int) T {
+	var zero T
+	if r < 0 || c < 0 {
+		return zero
+	}
+	if r >= len(b) {
+		return zero
+	}
+	if c >= len(b[r]) {
+		return zero
+	}
+	return b[r][c]
+}
+
+func (b *Board[T]) Set(r, c int, val T) {
+	if r < 0 || c < 0 {
+		return
+	}
+	if r >= len(*b) {
+		return
+	}
+	if c >= len((*b)[r]) {
+		return
+	}
+	(*b)[r][c] = val
+}
+
+func (b Board[T]) GetCoord(co Coord) T {
+	return b.Get(co.R(), co.C())
+}
+
+func (b *Board[T]) SetCoord(co Coord, val T) {
+	b.Set(co.R(), co.C(), val)
+}
+
+func (b Board[T]) AllCoords() []Coord {
+	var coords []Coord
+	for r := range b {
+		for c := range b[r] {
+			coords = append(coords, MakeCoord(r, c))
+		}
+	}
+	return coords
+}
+
+func (b Board[T]) InBounds(co Coord) bool {
+	if co.R() < 0 || co.C() < 0 {
+		return false
+	}
+	if co.R() >= len(b) {
+		return false
+	}
+	if co.C() >= len(b[co.R()]) {
+		return false
+	}
+	return true
+}
+
+func (b Board[T]) Neighbors4(co Coord) []Coord {
+	var coords []Coord
+	for _, diff := range [][2]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} {
+		dco := co.Apply(diff)
+		if !b.InBounds(dco) {
+			continue
+		}
+		coords = append(coords, dco)
+	}
+	return coords
+}
+
+func (b Board[T]) Neighbors8(co Coord) []Coord {
+	var coords []Coord
+	for _, diff := range [][2]int{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}} {
+		dco := co.Apply(diff)
+		if !b.InBounds(dco) {
+			continue
+		}
+		coords = append(coords, dco)
+	}
+	return coords
+}
