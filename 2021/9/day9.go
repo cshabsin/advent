88f7fca7338fe7d0d0caf4c0bca22af9371b068f
Chris Shabsin <cshabsin@gmail.com>
2021-12-11 21:20:35 -0500

    Port day9 to use shared rectangular board

--- 2021/9/day9.go
+++ 2021/9/day9.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"log"
 	"sort"
+	"strconv"
 
+	"github.com/cshabsin/advent/commongen/board"
 	"github.com/cshabsin/advent/commongen/readinp"
+	"github.com/cshabsin/advent/commongen/set"
 )
 
 func main() {
@@ -16,26 +19,34 @@ func main() {
 	day9b("input.txt")
 }
 
+type intS int
+
+func (i intS) String() string {
+	return strconv.Itoa(int(i))
+}
+
+func (intS) Delimiter() string {
+	return "" // no space between entries
+}
+
 func day9a(fn string) {
 	ch, err := readinp.Read(fn, parse)
 	if err != nil {
 		log.Fatal(err)
 	}
-	var board board
+	var brd board.Board[intS]
 	for line := range ch {
 		row, err := line.Get()
 		if err != nil {
 			log.Fatal(err)
 		}
-		board = append(board, row)
+		brd = append(brd, row)
 	}
-	var risk int
-	for r := range board {
-		for c := range board[r] {
-			risk += board.risk(r, c)
-		}
+	var rsk intS
+	for _, co := range brd.AllCoords() {
+		rsk += risk(brd, co)
 	}
-	fmt.Println(risk)
+	fmt.Println(rsk)
 }
 
 func day9b(fn string) {
@@ -43,96 +54,72 @@ func day9b(fn string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	var board board
+	var brd board.Board[intS]
 	for line := range ch {
 		row, err := line.Get()
 		if err != nil {
 			log.Fatal(err)
 		}
-		board = append(board, row)
+		brd = append(brd, row)
 	}
-	var lows [][2]int
-	for r := range board {
-		for c := range board[r] {
-			if board.isLow(r, c) {
-				lows = append(lows, [2]int{r, c})
-			}
+	var lows []board.Coord
+	for _, co := range brd.AllCoords() {
+		if isLow(brd, co) {
+			lows = append(lows, co)
 		}
 	}
 	var basins []int
 	for _, low := range lows {
-		r, c := low[0], low[1]
-		basins = append(basins, board.basinSize(r, c))
+		basins = append(basins, basinSize(brd, low))
 	}
 	sort.Sort(sort.IntSlice(basins))
 	fmt.Println(basins)
 }
 
-type board [][]int
-
-func (b board) get(r, c int) int {
-	if r < 0 || c < 0 {
-		return 9 // the outer edge is "higher" than everything
-	}
-	if r >= len(b) {
-		return 9
-	}
-	if c >= len(b[r]) {
-		return 9
-	}
-	return b[r][c]
-}
-
-func (b board) isLow(r, c int) bool {
-	for dr := -1; dr <= 1; dr++ {
-		for dc := -1; dc <= 1; dc++ {
-			if dr == 0 && dc == 0 {
-				continue
-			}
-			if b.get(r+dr, c+dc) <= b.get(r, c) {
-				return false
-			}
+func isLow(b board.Board[intS], co board.Coord) bool {
+	for _, dco := range b.Neighbors8(co) {
+		if b.GetCoord(dco) <= b.GetCoord(co) {
+			return false
 		}
 	}
 	return true
 }
 
-func (b board) risk(r, c int) int {
-	if !b.isLow(r, c) {
+func risk(brd board.Board[intS], co board.Coord) intS {
+	if !isLow(brd, co) {
 		return 0
 	}
-	return b.get(r, c) + 1
+	return brd.GetCoord(co) + 1
 }
 
-func (b board) basinSize(r, c int) int {
-	return b.basinSizeInternal(r, c, visited{})
+func basinSize(brd board.Board[intS], co board.Coord) int {
+	return basinSizeInternal(brd, co, set.Set[board.Coord]{})
 }
 
-type visited map[int]bool
-
-func (v visited) isVisited(r, c int) bool {
-	return v[r*1000+c]
-}
-
-func (v visited) visit(r, c int) {
-	v[r*1000+c] = true
-}
-
-func (b board) basinSizeInternal(r, c int, v visited) int {
-	v.visit(r, c)
-	if b.get(r, c) == 9 {
+func basinSizeInternal(brd board.Board[intS], co board.Coord, v set.Set[board.Coord]) int {
+	v.Add(co)
+	if brd.GetCoord(co) == 9 {
 		return 0
 	}
 	basin := 1
-	for _, diff := range [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} {
-		dr := diff[0]
-		dc := diff[1]
-		if v.isVisited(r+dr, c+dc) {
+	for _, dco := range brd.Neighbors4(co) {
+		if v.Contains(dco) {
 			continue
 		}
-		if b.get(r+dr, c+dc) >= b.get(r, c) {
-			basin += b.basinSizeInternal(r+dr, c+dc, v)
+		if brd.GetCoord(dco) >= brd.GetCoord(co) {
+			basin += basinSizeInternal(brd, dco, v)
 		}
 	}
 	return basin
+}
+
+func parse(line string) ([]intS, error) {
+	var d []intS
+	for _, c := range line {
+		if c < '0' || c > '9' {
+			return nil, fmt.Errorf("in line %q, illegal character %c", line, c)
+		}
+		d = append(d, intS(c-'0'))
+	}
+	return d, nil
 }
