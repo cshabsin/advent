441dce5a4e2d9d436bba674867d41347cf75917e
Chris Shabsin <cshabsin@gmail.com>
2021-12-15 00:16:04 -0500

    big ol slow search

--- 2021/15/day15.go
+++ 2021/15/day15.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 
+	"github.com/cshabsin/advent/commongen/board"
 	"github.com/cshabsin/advent/commongen/readinp"
 )
 
@@ -17,24 +19,58 @@ func main() {
 
 func part1(fn string) {
 	fmt.Println("---", fn, ":")
-	_, err := load(fn)
+	brd, err := load(fn)
 	if err != nil {
 		log.Fatal(err)
 	}
+	totals := make(board.Board[intS], len(brd), len(brd))
+	for i := range totals {
+		totals[i] = make([]intS, len(brd[i]))
+	}
+	fmt.Println(traverse(brd, totals, board.MakeCoord(0, 0), 0))
 }
 
-func load(fn string) (string, error) {
-	ch, err := readinp.Read(fn, readinp.S)
-	if err != nil {
-		return "", err
+func traverse(brd, totals board.Board[intS], co board.Coord, risk intS) intS {
+	if co.R() == len(brd)-1 && co.C() == len(brd[0])-1 {
+		return risk
 	}
-	var acc string
-	for line := range ch {
-		s, err := line.Get()
-		if err != nil {
-			return "", err
+	var minRisk intS = 9999999
+	for _, dco := range brd.Neighbors4(co) {
+		if totals.GetCoord(dco) == 0 || totals.GetCoord(dco) > risk+brd.GetCoord(dco) {
+			totals.SetCoord(dco, risk+brd.GetCoord(dco))
+			subRisk := traverse(brd, totals, dco, risk+brd.GetCoord(dco))
+			if subRisk < minRisk {
+				minRisk = subRisk
+			}
 		}
-		acc += s
+	}
+	return minRisk
+}
+
+func load(fn string) (board.Board[intS], error) {
+	ch, err := readinp.Read(fn, func(s string) ([]intS, error) {
+		var rc []intS
+		for _, c := range s {
+			rc = append(rc, intS(c)-'0')
+		}
+		return rc, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	var acc board.Board[intS]
+	for line := range ch {
+		row, err := line.Get()
+		if err != nil {
+			return nil, err
+		}
+		acc = append(acc, row)
 	}
 	return acc, nil
+}
+
+type intS int
+
+func (i intS) String() string {
+	return strconv.Itoa(int(i))
 }
