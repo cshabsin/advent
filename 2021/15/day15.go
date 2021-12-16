fd6d794cfb35560bc6f97adf188cbeacfdaddec2
Chris Shabsin <cshabsin@gmail.com>
2021-12-15 23:40:29 -0500

    attempt to make it ... more efficient?

--- 2021/15/day15.go
+++ 2021/15/day15.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"sort"
 	"strconv"
 
 	"github.com/cshabsin/advent/commongen/board"
@@ -28,28 +29,81 @@ func part1(fn string, isQuint bool) {
 	if isQuint {
 		brd = quintuple(brd)
 	}
-	totals := make(board.Board[intS], len(brd), len(brd))
-	for i := range totals {
-		totals[i] = make([]intS, len(brd[i]))
+	unvisited := distanceBoard{
+		ds:        make(map[board.Coord]intS),
+		nexts:     make(map[intS][]board.Coord),
+		unvisited: make(map[board.Coord]bool),
 	}
-	fmt.Println(traverse(brd, totals, board.MakeCoord(0, 0), 0))
+	for _, co := range brd.AllCoords() {
+		unvisited.setRaw(co, 99999999999)
+	}
+	current := board.MakeCoord(0, 0)
+	target := board.MakeCoord(len(brd)-1, len(brd[0])-1)
+	unvisited.setRaw(current, 0)
+	for {
+		if current == target {
+			fmt.Println(unvisited.get(current))
+			return
+		}
+		for _, neigh := range brd.Neighbors4(current) {
+			if !unvisited.has(neigh) {
+				continue
+			}
+			unvisited.set(neigh, unvisited.get(current)+brd.GetCoord(neigh))
+		}
+		unvisited.remove(current)
+		current = unvisited.next()
+	}
 }
 
-func traverse(brd, totals board.Board[intS], co board.Coord, risk intS) intS {
-	if co.R() == len(brd)-1 && co.C() == len(brd[0])-1 {
-		return risk
+type distanceBoard struct {
+	ds        map[board.Coord]intS
+	unvisited map[board.Coord]bool
+
+	// map from distance to list of coordinates with that size
+	nexts map[intS][]board.Coord
+
+	// sorted list (smallest to largest) of distances in the next map
+	nextDistances []intS
+}
+
+func (d *distanceBoard) set(co board.Coord, dist intS) {
+	// fmt.Println("setting", co, "to", dist)
+	d.ds[co] = dist
+	if d.nexts[dist] == nil {
+		d.nextDistances = append(d.nextDistances, dist)
+		sort.Slice(d.nextDistances, func(i, j int) bool { return d.nextDistances[i] < d.nextDistances[j] })
 	}
-	var minRisk intS = 9999999
-	for _, dco := range brd.Neighbors4(co) {
-		if totals.GetCoord(dco) == 0 || totals.GetCoord(dco) > risk+brd.GetCoord(dco) {
-			totals.SetCoord(dco, risk+brd.GetCoord(dco))
-			subRisk := traverse(brd, totals, dco, risk+brd.GetCoord(dco))
-			if subRisk < minRisk {
-				minRisk = subRisk
-			}
-		}
+	d.nexts[dist] = append(d.nexts[dist], co)
+}
+
+func (d *distanceBoard) setRaw(co board.Coord, val intS) {
+	d.ds[co] = val
+	d.unvisited[co] = true
+	// don't put this distance into the nodes to consider for next current node.
+}
+
+func (d *distanceBoard) has(co board.Coord) bool {
+	return d.unvisited[co]
+}
+
+func (d *distanceBoard) get(co board.Coord) intS {
+	return d.ds[co]
+}
+
+func (d *distanceBoard) remove(co board.Coord) {
+	delete(d.unvisited, co)
+}
+
+func (d *distanceBoard) next() board.Coord {
+	dist := d.nextDistances[0]
+	next := d.nexts[dist][0]
+	d.nexts[dist] = d.nexts[dist][1:len(d.nexts[dist])]
+	if len(d.nexts[dist]) == 0 {
+		d.nextDistances = d.nextDistances[1:len(d.nextDistances)]
+		delete(d.nexts, dist)
 	}
-	return minRisk
+	return next
 }
 
 func load(fn string) (board.Board[intS], error) {
