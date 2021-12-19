e45a3beed2428009c9a7895fbb226dac99b702d4
Chris Shabsin <cshabsin@gmail.com>
2021-12-19 16:57:08 -0500

    Move AllRotations to matrix package

--- commongen/matrix/matrix.go
+++ commongen/matrix/matrix.go
@@ -1,5 +1,7 @@
 package matrix
 
+import "github.com/cshabsin/advent/commongen/set"
+
 var (
 	ident = Matrix{
 		{1, 0, 0},
@@ -48,4 +50,44 @@ func (m Matrix) dot(n Matrix, i, j int) int {
 		rc += m[i][k] * n[k][j]
 	}
 	return rc
+}
+
+func AllRotations() []Matrix {
+	if allRotations == nil {
+		allRotations = calcAllRotations()
+	}
+	return allRotations
+}
+
+var (
+	allRotations []Matrix
+)
+
+func calcAllRotations() []Matrix {
+	zRot := Matrix{ // rotate around z axis
+		{0, -1, 0},
+		{1, 0, 0},
+		{0, 0, 1},
+	}
+	yRot := Matrix{ //rotate around y axis
+		{0, 0, -1},
+		{0, 1, 0},
+		{1, 0, 0},
+	}
+	xRot := Matrix{ // rotate around x axis
+		{1, 0, 0},
+		{0, 0, -1},
+		{0, 1, 0},
+	}
+	rotSet := set.Set[Matrix]{}
+	rotSet.Add(Ident())
+	for x := 0; x < 4; x++ {
+		for y := 0; y < 4; y++ {
+			for z := 0; z < 4; z++ {
+				val := xRot.Pow(x).Mul(yRot.Pow(y)).Mul(zRot.Pow(z))
+				rotSet.Add(val)
+			}
+		}
+	}
+	return rotSet.AsSlice()
 }
