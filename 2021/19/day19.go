c211eaa72bde76045b93bff011fe9b4c66bc8365
Chris Shabsin <cshabsin@gmail.com>
2021-12-19 21:15:49 -0500

    pile of code. who knows what works?

--- 2021/19/day19.go
+++ 2021/19/day19.go
@@ -5,7 +5,9 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/cshabsin/advent/commongen/matrix"
 	"github.com/cshabsin/advent/commongen/readinp"
+	"github.com/cshabsin/advent/commongen/set"
 )
 
 func main() {
@@ -13,7 +15,92 @@ func main() {
 }
 
 func part1(fn string) {
-	fmt.Println(readScanners(fn))
+	sf := newScannerFinder(readScanners(fn))
+	fmt.Println("scannerfinder initialized")
+	for {
+		var numMatches int
+		for i := range sf.allScanners {
+			if sf.find(i) {
+				numMatches++
+				sf.isFound.Add(i)
+			} else {
+				fmt.Println("find", i, "false")
+			}
+		}
+		if numMatches == len(sf.allScanners) {
+			break
+		}
+	}
+}
+
+type scannerFinder struct {
+	// all beacon vectors precalculated for each scanner.
+	// arranged by scanner, then rotation (0-23), then by origin beacon.
+	// Note: allBeacons[*][*][x][x] == 0,0,0
+	allScanners [][][][]matrix.Vector3
+
+	foundScanners  []int              // each entry is an index into allScanners
+	isFound        set.Set[int]       // each allScanners index that has been added to foundScanners
+	foundOffsets   []matrix.Vector3   // each entry is the location of the given scanner relative to the "origin"
+	foundRotations []matrix.Matrix3x3 // each entry is the rotation vector of the given scanner
+}
+
+func newScannerFinder(rawScanners []scanner) *scannerFinder {
+	rc := &scannerFinder{
+		foundScanners:  []int{0},
+		isFound:        set.Set[int]{0: true},
+		foundOffsets:   []matrix.Vector3{matrix.Vector3{0, 0, 0}},
+		foundRotations: []matrix.Matrix3x3{matrix.Ident()},
+	}
+	var allScanners [][][][]matrix.Vector3
+	for _, scanner := range rawScanners {
+		var byScanner [][][]matrix.Vector3
+		for _, rot := range matrix.AllRotations() {
+			var rotBeacons []matrix.Point3
+			for _, beacon := range scanner.beacons {
+				rotBeacons = append(rotBeacons, beacon.Mul(rot))
+			}
+			var byOrigin [][]matrix.Vector3
+			for _, origin := range rotBeacons {
+				var beacons []matrix.Vector3
+				for _, target := range rotBeacons {
+					beacons = append(beacons, target.Sub(origin))
+				}
+				byOrigin = append(byOrigin, beacons)
+			}
+			byScanner = append(byScanner, byOrigin)
+		}
+		allScanners = append(allScanners, byScanner)
+	}
+	rc.allScanners = allScanners
+	return rc
+}
+
+func (s scannerFinder) find(tgtScanner int) bool {
+	if s.isFound[tgtScanner] {
+		return true
+	}
+
+	for _, cmpScanner := range s.foundScanners {
+		cmpScanners := s.allScanners[cmpScanner][0][0]
+		for rot, byOrigin := range s.allScanners[tgtScanner] {
+			for origin, tgtBeacons := range byOrigin {
+				var matches int
+				for _, tgtBeacon := range tgtBeacons {
+					for _, cmpBeacon := range cmpScanners {
+						if tgtBeacon.Eq(cmpBeacon) {
+							matches++
+						}
+					}
+				}
+				if matches > 12 {
+					fmt.Println("matched", cmpScanner, "to", tgtScanner, "with rot", rot, "and origin", origin)
+					return true
+				}
+			}
+		}
+	}
+	return false
 }
 
 func readScanners(fn string) []scanner {
@@ -27,8 +114,9 @@ func readScanners(fn string) []scanner {
 
 type scanner struct {
 	num     int
-	beacons [][3]int
+	beacons []matrix.Point3
 }
+
 type parser struct {
 	current scanner
 }
@@ -46,7 +134,7 @@ func (p *parser) Parse(line string) (scanner, bool, error) {
 		return scanner{}, false, nil
 	}
 	strs := strings.Split(line, ",")
-	p.current.beacons = append(p.current.beacons, [3]int{
+	p.current.beacons = append(p.current.beacons, matrix.Point3{
 		readinp.Atoi(strs[0]),
 		readinp.Atoi(strs[1]),
 		readinp.Atoi(strs[2]),
--- commongen/matrix/matrix.go
+++ commongen/matrix/matrix.go
@@ -3,21 +3,57 @@ package matrix
 import "github.com/cshabsin/advent/commongen/set"
 
 var (
-	ident = Matrix{
+	ident = Matrix3x3{
 		{1, 0, 0},
 		{0, 1, 0},
 		{0, 0, 1},
 	}
 )
 
-type Matrix [3][3]int
+// Point3 is a point in 3-space.
+type Point3 [3]int
 
-func Ident() Matrix {
+func (p Point3) Sub(q Point3) Vector3 {
+	return Vector3{q[0] - p[0], q[1] - p[1], q[2] - p[2]}
+}
+
+func (p Point3) Mul(rot Matrix3x3) Point3 {
+	return Point3(Vector3(p).Mul(rot))
+}
+
+// Vector3 is a vector in 3-space, or a difference between two points.
+type Vector3 [3]int
+
+func (v Vector3) Mul(rot Matrix3x3) Vector3 {
+	return Vector3{
+		v[0]*rot[0][0] + v[1]*rot[1][0] + v[2]*rot[2][0],
+		v[0]*rot[0][1] + v[1]*rot[1][1] + v[2]*rot[2][1],
+		v[0]*rot[0][2] + v[1]*rot[1][2] + v[2]*rot[2][2],
+	}
+}
+
+func (v Vector3) Eq(w Vector3) bool {
+	return v[0] == w[0] && v[1] == w[1] && v[2] == w[2]
+}
+
+type Matrix3x3 [3][3]int
+
+func Ident() Matrix3x3 {
 	return ident.Clone()
 }
 
-func (m Matrix) Clone() Matrix {
-	rc := Matrix{}
+func (m Matrix3x3) Transpose() Matrix3x3 {
+	rc := Matrix3x3{}
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			rc[i][j] = m[j][i]
+		}
+	}
+	return rc
+}
+
+func (m Matrix3x3) Clone() Matrix3x3 {
+	rc := Matrix3x3{}
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 3; j++ {
 			rc[i][j] = m[i][j]
@@ -26,8 +62,8 @@ func (m Matrix) Clone() Matrix {
 	return rc
 }
 
-func (m Matrix) Mul(n Matrix) Matrix {
-	rc := Matrix{}
+func (m Matrix3x3) Mul(n Matrix3x3) Matrix3x3 {
+	rc := Matrix3x3{}
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 3; j++ {
 			rc[i][j] = m.dot(n, i, j)
@@ -36,7 +72,7 @@ func (m Matrix) Mul(n Matrix) Matrix {
 	return rc
 }
 
-func (m Matrix) Pow(n int) Matrix {
+func (m Matrix3x3) Pow(n int) Matrix3x3 {
 	rc := ident
 	for i := 0; i < n; i++ {
 		rc = rc.Mul(m)
@@ -44,7 +80,7 @@ func (m Matrix) Pow(n int) Matrix {
 	return rc
 }
 
-func (m Matrix) dot(n Matrix, i, j int) int {
+func (m Matrix3x3) dot(n Matrix3x3, i, j int) int {
 	var rc int
 	for k := 0; k < 3; k++ {
 		rc += m[i][k] * n[k][j]
@@ -52,34 +88,38 @@ func (m Matrix) dot(n Matrix, i, j int) int {
 	return rc
 }
 
-func AllRotations() []Matrix {
+func AllRotations() []Matrix3x3 {
 	if allRotations == nil {
 		allRotations = calcAllRotations()
 	}
 	return allRotations
 }
 
+func Rotation(i int) Matrix3x3 {
+	return AllRotations()[i]
+}
+
 var (
-	allRotations []Matrix
+	allRotations []Matrix3x3
 )
 
-func calcAllRotations() []Matrix {
-	zRot := Matrix{ // rotate around z axis
+func calcAllRotations() []Matrix3x3 {
+	zRot := Matrix3x3{ // rotate around z axis
 		{0, -1, 0},
 		{1, 0, 0},
 		{0, 0, 1},
 	}
-	yRot := Matrix{ //rotate around y axis
+	yRot := Matrix3x3{ //rotate around y axis
 		{0, 0, -1},
 		{0, 1, 0},
 		{1, 0, 0},
 	}
-	xRot := Matrix{ // rotate around x axis
+	xRot := Matrix3x3{ // rotate around x axis
 		{1, 0, 0},
 		{0, 0, -1},
 		{0, 1, 0},
 	}
-	rotSet := set.Set[Matrix]{}
+	rotSet := set.Set[Matrix3x3]{}
 	rotSet.Add(Ident())
 	for x := 0; x < 4; x++ {
 		for y := 0; y < 4; y++ {
