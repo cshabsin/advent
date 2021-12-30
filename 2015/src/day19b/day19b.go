dec58a61ee405fa276a830ae083012fcd223de17
Chris Shabsin <cshabsin@gmail.com>
2021-12-30 00:34:55 -0500

    try to implement 2015-19 again, get heap helper

--- 2015/src/day19b/day19b.go
+++ 2015/src/day19b/day19b.go
@@ -8,6 +8,8 @@ import (
 	"os"
 	"strings"
 	"unicode"
+
+	"github.com/cshabsin/advent/commongen/heapof"
 )
 
 func main() {
@@ -61,12 +63,104 @@ func part2(fn string) {
 	}
 	fmt.Println(dict)
 	fmt.Println(transforms)
-	fmt.Println(dict.ToString(target))
+	fmt.Println(target)
+
+	for _, s := range []string{
+		"e",
+		"CRnCa",
+		"CRnCaArArMg",
+		"CRnCaArArMgP",
+		target.String(),
+	} {
+		fmt.Println(s, ":", formula{
+			form:   dict.Parse(s),
+			target: target.form,
+			dict:   dict,
+		}.Cost())
+	}
+	tgtStr := target.String()
+	initial := &formula{
+		dict:   dict,
+		form:   []int{0},
+		target: target.form,
+	} // "e"
+	formHeap := heapof.Make([]*formula{initial})
+	i := 0
+	for {
+		if i%100 == 0 {
+			fmt.Println(i, "states:", formHeap.Len())
+		}
+		i++
+		if formHeap.Len() == 0 {
+			fmt.Println("out of states!")
+		}
+		form := formHeap.PopHeap()
+		nexts := transforms.nexts(form)
+		for _, next := range nexts {
+			if next.String() == tgtStr {
+				fmt.Println("win!")
+				fmt.Println(next, "steps:", next.steps)
+				return
+			}
+			formHeap.PushHeap(next)
+		}
+	}
 }
 
+type formula struct {
+	dict   *dictionary
+	form   []int
+	target []int
+	steps  int // number of steps to get here
+}
+
+func (f formula) Cost() int {
+	cost := f.steps
+	var fi, ti int
+	var prevMatches int // prefer streaks
+	for ti < len(f.target) {
+		if f.form[fi] == f.form[ti] {
+			cost -= prevMatches
+			prevMatches++
+			fi++
+			ti++
+			if fi >= len(f.form) {
+				// we've matched the whole input, now add the length
+				// of the target's remainder
+				cost += len(f.target) - ti
+				break
+			}
+			continue
+		}
+		prevMatches = 0
+		cost++
+		ti++
+	}
+	return cost
+}
+
+func (f formula) String() string {
+	return f.dict.ToString(f.form)
+}
+
+type transforms map[int][]transform
 type transform []int
 
-func ReadInput(fn string) (*dictionary, map[int][]transform, []int, error) {
+func (t transforms) nexts(from *formula) []*formula {
+	var out []*formula
+	for i, elem := range from.form {
+		for _, tr := range t[elem] {
+			var nextVals []int
+			nextVals = append(nextVals, from.form[0:i]...)
+			nextVals = append(nextVals, tr...)
+			nextVals = append(nextVals, from.form[i+1:len(from.form)]...)
+			out = append(out, &formula{dict: from.dict, form: nextVals})
+		}
+	}
+	return out
+}
+
+func ReadInput(fn string) (*dictionary, transforms, *formula, error) {
 	f, err := os.Open(fn)
 	if err != nil {
 		log.Fatal(err)
@@ -104,5 +198,9 @@ func ReadInput(fn string) (*dictionary, map[int][]transform, []int, error) {
 		return nil, nil, nil, errors.New("file ended during phase 2 of parsing")
 	}
 	target := dict.Parse(scanner.Text())
-	return dict, transforms, target, nil
+	return dict, transforms, &formula{
+		dict:   dict,
+		form:   target,
+		target: target,
+	}, nil
 }
---
+++ commongen/heapof/heapof.go
@@ -0,0 +1,47 @@
+package heapof
+
+import "container/heap"
+
+type coster interface {
+	Cost() int
+}
+
+func Make[T coster](initElems []T) *Heap[T] {
+	return &Heap[T]{elems: initElems}
+}
+
+type Heap[T coster] struct {
+	elems []T
+}
+
+func (h *Heap[T]) Len() int {
+	return len(h.elems)
+}
+
+func (h *Heap[T]) Less(i, j int) bool {
+	return h.elems[i].Cost() < h.elems[j].Cost()
+}
+
+func (h *Heap[T]) Swap(i, j int) {
+	tmp := h.elems[i]
+	h.elems[i] = h.elems[j]
+	h.elems[j] = tmp
+}
+
+func (h *Heap[T]) Push(x interface{}) {
+	h.elems = append(h.elems, x.(T))
+}
+
+func (h *Heap[T]) PushHeap(t T) {
+	heap.Push(h, t)
+}
+
+func (h *Heap[T]) PopHeap() T {
+	return heap.Pop(h).(T)
+}
+
+func (h *Heap[T]) Pop() interface{} {
+	tmp := h.elems[h.Len()-1]
+	h.elems = h.elems[:h.Len()-1]
+	return tmp
+}
