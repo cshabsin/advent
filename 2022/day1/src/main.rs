8854be4e915a123b4793b0b6feded63a6a2daeef
Chris Shabsin <cshabsin@gmail.com>
2022-12-01 00:11:50 -0500

    day 2

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -18,11 +18,19 @@ fn main() {
     if current_elf != 0 {
         elves.push(current_elf);
     }
-    let mut max = 0;
+    let mut max: [i32; 3] = [0,0,0];
     for elf in &elves {
-        if elf > &max {
-            max = *elf;
+        if elf > &max[0] {
+            max[2] = max[1];
+            max[1] = max[0];
+            max[0] = *elf;
+        } else if elf > &max[1] {
+            max[2] = max[1];
+            max[1] = *elf;
+        } else if elf > &max[2] {
+            max[2] = *elf;
         }
     }
-    println!("{max}");
+    let total = max[0]+max[1]+max[2];
+    println!("{total}");
 }
