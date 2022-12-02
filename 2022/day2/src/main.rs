476b6899caf88c25fea0ebc21ccbcf28f08cd6b9
Chris Shabsin <cshabsin@gmail.com>
2022-12-02 00:25:08 -0500

    day 2 part 2

--- 2022/day2/src/main.rs
+++ 2022/day2/src/main.rs
@@ -3,9 +3,16 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let lines: i32 = fs::read_to_string(&args[1]).expect("should have been able to read the file")
-    .split("\n")
-    .filter(|x| (*x) != "").map(score).sum();
+    let mut f: fn(&str) -> i32 = score;
+    if args.len() > 2 {
+        f = score2;
+    }
+    let lines: i32 = fs::read_to_string(&args[1])
+        .expect("should have been able to read the file")
+        .split("\n")
+        .filter(|x| (*x) != "")
+        .map(f)
+        .sum();
     println!("{lines}")
 }
 
@@ -19,7 +26,9 @@ fn score(x: &str) -> i32 {
         'X' => 1,
         'Y' => 2,
         'Z' => 3,
-        _ => { panic!("unexpected me {me}"); }
+        _ => {
+            panic!("unexpected me {me}");
+        }
     };
     if x == "A X" {
         score += 3;
@@ -35,4 +44,53 @@ fn score(x: &str) -> i32 {
         score += 3;
     }
     score
+}
+
+fn score2(x: &str) -> i32 {
+    let mut iter = x.chars();
+    let opp = iter.next().unwrap();
+    iter.next();
+    let me = iter.next().unwrap();
+
+    match me {
+        'X' =>
+        // lose
+        {
+            match opp {
+                'A' => 3,
+                'B' => 1,
+                'C' => 2,
+                _ => {
+                    panic!("unknown opp {opp}");
+                }
+            }
+        }
+        'Y' =>
+        // draw
+        {
+            match opp {
+                'A' => 1 + 3,
+                'B' => 2 + 3,
+                'C' => 3 + 3,
+                _ => {
+                    panic!("unknown opp {opp}");
+                }
+            }
+        }
+        'Z' =>
+        // win
+        {
+            match opp {
+                'A' => 2 + 6,
+                'B' => 3 + 6,
+                'C' => 1 + 6,
+                _ => {
+                    panic!("unknown opp {opp}");
+                }
+            }
+        }
+        _ => {
+            panic!("unknown me {me}");
+        }
+    }
 }
